18691bb4d05822fcb00445eed6ed101fe759c81c
Eity2021 <Eity2021akter@gmail.com>
2022-07-02 05:05:28 +0600

    github login process

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import {
   getAuth,
+  GithubAuthProvider,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
 } from "firebase/auth";
+
 import app from "./firebase.init";
 import { useState } from "react";
 const auth = getAuth(app);
 
 function App() {
   const [user, setUser] = useState({});
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
+  //google provider
   const handleGoogleSignIn = () => {
     console.log("working");
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         const user = result.user;
         setUser(user);
-        console.log(user);
+        
       })
       .catch((error) => {
         console.log("error", error);
       });
   };
+  //github provider
+  const handleGithubSignIn = () => {
+ 
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const user = error.user;
+        setUser(user);
+      });
+  };
+
+  //google signOut
   const handleGoogleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -37,6 +56,21 @@ function App() {
         setUser({});
       });
   };
+
+  //github signOut
+  const handleGithubSignOut = () => {
+    console.log("click");
+    //const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        setUser({})
+      })
+      .catch((error) => {
+        // An error happened.
+        setUser({})
+      });
+  };
   return (
     <div className="App">
       <h1>Name : {user.displayName}</h1>
@@ -46,9 +80,15 @@ function App() {
       <br />
 
       {user.email ? (
-        <button onClick={handleGoogleSignOut}>Google Sign out</button>
+        <>
+          <button onClick={handleGoogleSignOut}>Google Sign out</button>
+          <button onClick={handleGithubSignOut}>Github Sign out</button>
+        </>
       ) : (
-        <button onClick={handleGoogleSignIn}>Google Sign in</button>
+        <>
+          <button onClick={handleGoogleSignIn}>Google Sign in</button>
+          <button onClick={handleGithubSignIn}>Github Sign in</button>
+        </>
       )}
 
       <br />
